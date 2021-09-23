1dc5e311b426de59010c97ccdc4cd9636213651b
Carthur-P <carthur2263@gmail.com>
2021-09-24 11:33:38 +1200

    adding components

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './css/App.css';
+import Header from './components/Header'
+import Transaction from './components/Header'
+import Balance from './components/Header'
+import Breakdown from './components/Header'
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello World</h1>
+      <Header/>
     </div>
   );
 }
---
+++ expense-tracker/src/components/Transaction.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Transaction(){
+    return (
+        <div>
+            <h1>Expense Tracker</h1>
+        </div>
+    )
+}
+
+export default Transaction
