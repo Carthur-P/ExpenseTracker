25d8f80223ee639f49d90402f9bb7e715ddf806a
Carthur-P <carthur2263@gmail.com>
2021-10-01 15:27:01 +1300

    balance now show total amount from transaction

--- expense-tracker/src/components/Balance.js
+++ expense-tracker/src/components/Balance.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import '../css/Balance.css'
+import { TransactionContext } from '../context/transactionState';
 
 function Balance(){
+    const { transactions } = useContext(TransactionContext);
+    const amount = transactions.map(transaction => transaction.amount)
+    const total = amount.reduce((prevValue, currentValue) => prevValue + currentValue).toFixed(2);
+
     return (
         <div className="balanceContainer">
             <h4>YOUR BALANCE</h4>
-            <p>$0.00</p>
+            <p>{total > 0 ? `$${total}` : `- $${Math.abs(total)}`}</p>
         </div>
     )
 }
