ab3e8af9cbd10ca1e99940e41af90f4ad5ffbb7e
Carthur-P <carthur2263@gmail.com>
2022-03-09 15:34:36 +1300

    added transaction now shows up in the breakdown section

--- src/components/Breakdown.js
+++ src/components/Breakdown.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from 'react-redux';
 import '../css/Breakdown.css';
 
 function Breakdown() {
+  const [positive, setPositive] = useState(0);
+  const [negative, setNegative] = useState(0);
+
+  const state = useSelector(state => state.transaction);
+
+  function calculateBreakdown() {
+    let pTotal = 0
+    let nTotal = 0
+    state.map((t) => {
+      t.amount >= 0 ?
+        pTotal = pTotal + parseInt(t.amount) :
+        nTotal = nTotal + parseInt(t.amount)
+    });
+    setPositive(pTotal);
+    setNegative(Math.abs(nTotal));
+  }
+
+  useEffect(() => {
+    calculateBreakdown();
+  }, [state]);
+
   return (
     <div className="breakdownContainer">
       <div>
         <h5>INCOME</h5>
-        <p>+$0.00</p>
+        <p>+${positive}</p>
       </div>
       <div>
         <h5>EXPENSE</h5>
-        <p>-$0.00</p>
+        <p>-${negative}</p>
       </div>
     </div>
   );
