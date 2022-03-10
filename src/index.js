3d41d5098b12e874b9d4fdab8d50021464a56786
Carthur-P <carthur2263@gmail.com>
2022-03-11 11:23:37 +1300

    Transaction now persist across sessions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { store } from './state/store';
+import { store, persistor } from './state/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <PersistGate persistor={persistor}>
+      <React.StrictMode>
+        <App/>
+      </React.StrictMode>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- src/state/store.js
+++ src/state/store.js
@@ -1,4 +1,13 @@
 import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { reducers } from './reducers/index';
 
-export const store = createStore(reducers);
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistReducers = persistReducer(persistConfig, reducers)
+export const store = createStore(persistReducers);
+export const persistor = persistStore(store);
